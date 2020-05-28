bb1213ee944caa442b4fe2661261388f2697dc6d
Thong <thongnguyen1292@gmail.com>
2020-05-28 00:00:04 -0700

    fix printout label

--- src/toygenerator.cpp
+++ src/toygenerator.cpp
@@ -38,15 +38,15 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 void check_args(int argc, char **argv){
     
     #if GOF
-        if (argc < 6){
+        if (argc < 5){
             std::cerr << "Incorrect number of arguments.\n";
-            std::cerr << "Arguments: <number of bins> <background template file> <observed data file> <number of toys> <output file> [-optional: GPU only]\n";
+            std::cerr << "Arguments: <number of bins> <background template file> <observed data file> <number of toys> [--out <output file> (optional)] [--GPUonly <0 or 1> (optional)]\n";
             exit(EXIT_FAILURE);
         }
     #else
-        if (argc < 7){
+        if (argc < 6){
             std::cerr << "Incorrect number of arguments.\n";
-            std::cerr << "Arguments: <number of bins> <background template file> <signal template file> <observed data file> <number of toys> <output file> [-optional: GPU only]\n";
+            std::cerr << "Arguments: <number of bins> <background template file> <signal template file> <observed data file> <number of toys> [--out <output file> (optional)] [--GPUonly <0 or 1> (optional)]\n";
             exit(EXIT_FAILURE);
         }
     #endif
@@ -81,29 +81,47 @@ int frequentist_test(int argc, char **argv){
     //                GETTING INPUTS
     // ******************************************************
     
-    bool GPUonly = false;
-
     #if GOF == 0
-        std::string out_filename = argv[6];
         float ntoys = std::stof(argv[5]); // number of toy experiments
         std::string obs_filename = argv[4];
         std::string sig_filename = argv[3];
-        if (argc > 6) 
-        {
-            GPUonly = std::stoi(argv[7]);
-            std::cout << "Generating " << ntoys << " toy experiments to obtain the test statistics distribution on GPU only." << std::endl;
-        }
-            
     #else
-        std::string out_filename = argv[5];
         float ntoys = std::stof(argv[4]); // number of toy experiments
         std::string obs_filename = argv[3];
-        if (argc > 5) 
-        {
-            GPUonly = std::stoi(argv[6]);
-            std::cout << "Generating " << ntoys << " toy experiments to obtain the test statistics distribution on GPU only." << std::endl;
-        }
     #endif
+    
+    // Get output and GPU argument, if any
+    std::string out_filename;
+    int GPUonly = 0;
+
+    for (int i = 1; i < argc; ++i) 
+    {
+        if (std::string(argv[i]) == "--out") 
+        {
+            if (i + 1 < argc) 
+            { 
+                out_filename = argv[i++]; 
+            } 
+            else 
+            { 
+                std::cerr << "--out option requires one [string] argument." << std::endl;
+                return 1;
+            }
+        }
+        else if (std::string(argv[i]) == "--GPUonly")
+        {
+            if (i+1 < argc)
+            { 
+                GPUonly = std::stoi(argv[i+1]);
+                std::cout << "Generating " << ntoys << " toy experiments to obtain the test statistics distribution on GPU only." << std::endl;
+            }
+            else
+            {
+                std::cerr << "--GPUonly option requires one [integer] argument." << std::endl;
+                return 1;
+            }
+        } 
+    }
 
     static const int n_bins = std::stoi(argv[1]);
 
@@ -126,7 +144,7 @@ int frequentist_test(int argc, char **argv){
     float *obs_data = (float*) malloc(sizeof(float) * n_bins);
     if (obs_file.is_open())
     {
-        std::cout << "Reading " << n_bins << " bins from data file " << argv[2] << std::endl;
+        std::cout << "Reading " << n_bins << " bins from data file " << obs_filename << std::endl;
         for (int i =0; i < n_bins; i++)
             obs_file >> obs_data[i];
     }
@@ -140,7 +158,7 @@ int frequentist_test(int argc, char **argv){
         float *sig_expected = (float*) malloc(sizeof(float) * n_bins);
         if (sig_file.is_open())
         {
-            std::cout << "Reading " << n_bins << " bins from signal file " << argv[2] << std::endl;
+            std::cout << "Reading " << n_bins << " bins from signal file " << sig_filename << std::endl;
             for (int i =0; i < n_bins; i++)
                 sig_file >> sig_expected[i];
         }
@@ -174,7 +192,7 @@ int frequentist_test(int argc, char **argv){
     #endif
     }
     float *q_toys;
-    if (!GPUonly)
+    if (GPUonly == 0)
     {
         START_TIMER();
 
@@ -242,6 +260,8 @@ int frequentist_test(int argc, char **argv){
     curandState *devStates;
     float *dev_q_toys;
     float *host_q_toys = (float*) malloc(sizeof(float) * ntoys);
+    memset(host_q_toys, 0, ntoys * sizeof(float));
+
     int batch_toys = 0, nbatches = 0;
     
     // Determine whether can generate in 1 run or multiple chunks
@@ -253,9 +273,12 @@ int frequentist_test(int argc, char **argv){
         nbatches = ceil(ntoys / batch_toys); // Number of batches
     }
 
-    if (batch_toys == 0) // Do everything in 1 run
+    if (nbatches == 0) // Do everything in 1 run
     {
         CUDA_CALL(cudaMalloc((void **) &dev_q_toys, ntoys * sizeof(float)));
+        
+        // Set the result arrays to zero
+        CUDA_CALL(cudaMemset(dev_q_toys, 0, ntoys * sizeof(float)));
         
         // Get available memory 
         CUDA_CALL(cudaMemGetInfo(&freeMem, &totalMem));
@@ -294,9 +317,6 @@ int frequentist_test(int argc, char **argv){
                 n_bins * sizeof(float), cudaMemcpyHostToDevice));
         #endif
         
-        // Set the result arrays to zero
-        CUDA_CALL(cudaMemset(dev_q_toys, 0, 
-                    ntoys * sizeof(float)));
 
         #if GOF
         cuda_call_generate_goodness_of_fit_toys(nBlocks,
@@ -353,14 +373,10 @@ int frequentist_test(int argc, char **argv){
                 batch_toys = ntoys - batch * batch_toys;
                 if (batch_toys < 1) break;
             }
-            std::cout << "Batch " << batch+1 << " of " << nbatches << ": Generating " << batch_toys << " toys\n";
+            std::cout << "+ Batch " << batch+1 << " of " << nbatches << ": Generating " << batch_toys << " toys\n";
 
             CUDA_CALL(cudaMalloc((void **) &dev_q_toys, batch_toys * sizeof(float)));
-
-            #if GOF==0
-                float *dev_sig_expected;
-                CUDA_CALL(cudaMalloc((void **) &dev_sig_expected, n_bins * sizeof(float)));
-            #endif
+            CUDA_CALL(cudaMemset(dev_q_toys, 0, batch_toys * sizeof(float)));
 
             // Get available memory 
             CUDA_CALL(cudaMemGetInfo(&freeMem, &totalMem));
@@ -385,11 +401,8 @@ int frequentist_test(int argc, char **argv){
             CUDA_CALL(cudaMalloc((void **) &devStates, nStates * sizeof(curandState)));
             
             // Generating toys
-            printf("+  Using %d blocks with %d threads per block\n", nBlocks, threadsPerBlock);
+            printf("\t-- Using %d blocks with %d threads per block\n", nBlocks, threadsPerBlock);
             
-            // Set the result arrays to zero
-            CUDA_CALL(cudaMemset(dev_q_toys, 0, 
-                        batch_toys * sizeof(float)));
 
             #if GOF
             cuda_call_generate_goodness_of_fit_toys(nBlocks,
@@ -428,10 +441,49 @@ int frequentist_test(int argc, char **argv){
     // ******************************************************
     //            COMPARE AND SAVE RESULTS
     // ******************************************************
-    if (!GPUonly)
+    
+    std::ofstream file_cpu, file_gpu;
+    std::string out_cpu, out_gpu;
+    
+    if (GPUonly == 0)
     {
         int larger_cpu = 0;
         int larger_gpu = 0;
+
+        std::cout << "Toy-generation run time: \n";
+        std::cout << "+ On CPU: " << cpu_time_ms << " ms\n";
+        std::cout << "+ On GPU: " << gpu_time_ms << " ms\n";
+        float speed_up = cpu_time_ms/gpu_time_ms;
+        printf("Gained a %.0f-time speedup with GPU\n", speed_up);
+       
+        if (!out_filename.empty()) 
+        {
+            out_cpu = out_filename;
+            out_gpu = out_filename;
+            out_cpu.append("."+std::to_string(q_obs)+".cpu");
+            out_gpu.append("."+std::to_string(q_obs)+".gpu");
+            std::cout << "Saving the toy experiments' test statistics to " << out_cpu << " and " << out_gpu << std::endl;
+            file_cpu.open(out_cpu);
+            file_gpu.open(out_gpu);
+        }
+        tqdm bar2;
+        for (int i = 0; i < ntoys; i++)
+        {
+            bar2.progress(i, ntoys);
+            if (!out_filename.empty()) 
+            {
+                file_cpu << q_toys[i] << "\n";
+                file_gpu << host_q_toys[i] << "\n";
+            }
+            if (q_toys[i] > q_obs) larger_cpu++;
+            if (host_q_toys[i] > q_obs) larger_gpu++;
+        }
+        bar2.finish();
+        if (!out_filename.empty()) 
+        {
+            file_cpu.close();
+            file_gpu.close();
+        }
         float pval_cpu = float(larger_cpu)/ntoys;
         float pval_gpu = float(larger_gpu)/ntoys;
         #if GOF
@@ -448,33 +500,6 @@ int frequentist_test(int argc, char **argv){
         else
             std::cout << pval_gpu << " (GPU)\n";
 
-        std::cout << "Toy-generation run time: \n";
-        std::cout << "+ On CPU: " << cpu_time_ms << " ms\n";
-        std::cout << "+ On GPU: " << gpu_time_ms << " ms\n";
-        float speed_up = cpu_time_ms/gpu_time_ms;
-        printf("Gained a %.0f-time speedup with GPU\n", speed_up);
-        
-        std::string out_cpu = out_filename;
-        std::string out_gpu = out_filename;
-        out_cpu.append("."+std::to_string(q_obs)+".cpu");
-        out_gpu.append("."+std::to_string(q_obs)+".gpu");
-        std::cout << "Saving the toy experiments' test statistics to " << out_cpu << " and " << out_gpu << std::endl;
-        std::ofstream file_cpu, file_gpu;
-        file_cpu.open(out_cpu);
-        file_gpu.open(out_gpu);
-        tqdm bar2;
-        for (int i = 0; i < ntoys; i++)
-        {
-            bar2.progress(i, ntoys);
-            file_cpu << q_toys[i] << "\n";
-            file_gpu << host_q_toys[i] << "\n";
-            if (q_toys[i] > q_obs) larger_cpu++;
-            if (host_q_toys[i] > q_obs) larger_gpu++;
-        }
-        bar2.finish();
-        file_cpu.close();
-        file_gpu.close();
-
         // Free memory on host
         free(host_q_toys);
         free(bkg_expected);
@@ -488,6 +513,25 @@ int frequentist_test(int argc, char **argv){
     else
     {
         int larger_gpu = 0;
+
+        std::cout << "Toy-generation run time on GPU: " << gpu_time_ms << " ms\n";
+        
+        if (!out_filename.empty()) 
+        {
+            out_gpu = out_filename;
+            out_gpu.append("."+std::to_string(q_obs)+".gpu");
+            std::cout << "Saving the toy experiments' test statistics to " << out_gpu << std::endl;
+            file_gpu.open(out_gpu);
+        }
+        tqdm bar2;
+        for (int i = 0; i < ntoys; i++)
+        {
+            bar2.progress(i, ntoys);
+            if (host_q_toys[i] > q_obs) larger_gpu++;
+            if (!out_filename.empty()) file_gpu << host_q_toys[i] << "\n";
+        }
+        bar2.finish();
+        if (!out_filename.empty()) file_gpu.close();
         float pval_gpu = float(larger_gpu)/ntoys;
         #if GOF
             std::cout << "p-value from Goodness-of-fit test: ";
@@ -498,23 +542,6 @@ int frequentist_test(int argc, char **argv){
             std::cout << "less than " << 1/ntoys << " (GPU)\n";
         else
             std::cout << pval_gpu << " (GPU)\n";
-
-        std::cout << "Toy-generation run time on GPU: " << gpu_time_ms << " ms\n";
-        
-        std::string out_gpu = out_filename;
-        out_gpu.append("."+std::to_string(q_obs)+".gpu");
-        std::cout << "Saving the toy experiments' test statistics to " << out_gpu << std::endl;
-        std::ofstream file_gpu;
-        file_gpu.open(out_gpu);
-        tqdm bar2;
-        for (int i = 0; i < ntoys; i++)
-        {
-            bar2.progress(i, ntoys);
-            if (host_q_toys[i] > q_obs) larger_gpu++;
-            file_gpu << host_q_toys[i] << "\n";
-        }
-        bar2.finish();
-        file_gpu.close();
         
 
     }
